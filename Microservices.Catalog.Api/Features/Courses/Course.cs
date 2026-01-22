214508e1971fd1ff328b197442670a126996a525
omeraydin41 <omerenver1317@gmail.com>
2026-01-22 20:22:34 +0300

    12. ders notları

---
+++ Microservices.Catalog.Api/Features/Courses/Course.cs
@@ -0,0 +1,23 @@
+﻿using Microservices.Catalog.Api.Features.Categories;
+using Microservices.Catalog.Api.Repostories;
+
+namespace Microservices.Catalog.Api.Features.Courses
+{
+    public class Course : BaseEntity//Repostories klasorundaki BaseEntity den kalıtım alıyor : içi   [BsonElement("_id")]public Guid Id { get; set; }
+    {
+        public string Name { get; set; } = default!;//kurs adı // bu alanı null bırakma demek HATA VERMEZ KODU AÇIKLAR 
+        public string Description { get; set; } = default!;//açıklaması 
+        public decimal Price { get; set; }//fiyatı 
+        public Guid UserId { get; set; }//Kursu satın alanlar için oluşan alan
+        public string? Picture { get; set; }//kursun resmi ? null olabılır 
+        //strıng zaten null burdakı amacı artık nullable ozellığı açık gelir . Compiler null olabılecek yerlerde uyarı verir.null olabılır
+        //diyerek compiler a uyarı verme deriz . ekip içi çalışmada yardımcı olur. hangı değişkenlerin doldurulması konusunda.
+
+
+
+        public DateTime Created { get; set; }//kursun oluşturulma tarihi
+        public Guid CategoryId { get; set; }//kursun kategorisi
+        public Category Category { get; set; } = default!; //her kursun bir kategorisi olmak zorunda 
+        //Course den Category e bağlanmak için her kurs kategoriye bağlı olacak ef core yaklaşımı 
+    }
+}
