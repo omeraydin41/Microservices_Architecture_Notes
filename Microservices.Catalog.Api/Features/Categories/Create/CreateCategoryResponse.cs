ea0dbc5b140a93f84b2a3e3882ce5e2888ae70b7
omeraydin41 <omerenver1317@gmail.com>
2026-01-29 12:09:40 +0300

    Kaydedilmeden Push yaptığımdan kodlarım eksik görünmesinden kaynaklı temiz ilerlemek için ikinci Push İşlemi

--- Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryResponse.cs
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Microservices.Catalog.Api.Features.Categories.Create
 {
-    public record CreateCategoryResponse(int Id);//geriye DB de oluşan datanın ıd si dönecek 
+    public record CreateCategoryResponse(Guid Id);//geriye DB de oluşan datanın ıd si dönecek 
     //record dan dolayı immutabe class : int Id  propetıysınde set kısmı init olan ve contructorda bu değeri bekleyen 
     //bir kez alındığında değiştirilemez yapı 
 
