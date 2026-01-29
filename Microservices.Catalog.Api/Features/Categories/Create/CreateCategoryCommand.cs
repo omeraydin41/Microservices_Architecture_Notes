540918da5287ae38bcf59aa01b665e4b001c7deb
omeraydin41 <omerenver1317@gmail.com>
2026-01-29 12:00:41 +0300

    Create Category EndPointi yazdım girilen kategori adını kontrol edip varsa eğer SharedLib(tüm projedeki ortak kullanılan nesneler) Erro  mesajlarından uygun olanı doldurdum .Eğer girilen kategori yoksa da yenı kategori oluşturup bunu veri tabanına ekleyip kaydettim ve yine SharedLib den uygun olan Succes mesajını doldurdum .

---
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryCommand.cs
@@ -0,0 +1,26 @@
+﻿using NewMicroservices.Shared;
+
+namespace Microservices.Catalog.Api.Features.Categories.Create
+{
+    public record CreateCategoryCommand(string Name) : MediatR.IRequest<ServiceResult<CreateCategoryResponse>>;//primary constructor olarak inşa edildi
+                                                                                                               //string Name : set kısmı init olan ve constuructor ile istenen alan oldu
+
+
+    //üstteki ile arasında bir fark yok 
+    //NESNE ÖRNEĞİ OLDUĞUNDA PROPERTYLERİNDE DEĞİŞİKLİK OLMAZ 
+    //public record X
+    //{
+    //    public string  Name { get; init; }//NESNE ÖRNEĞİ OLDUĞUNDA PROPERTYLERİNDE DEĞİŞİKLİK OLMAZ  BU YUZDEN SET = İNİT OLDU 
+
+    //    public X(string name)
+    //    {
+    //        Name = name;
+
+    //        var x = new X("kalem");//Namw propertysi değer aldımı daha da değişmez 
+    //        //x.Name="defter";//hata verir çünkü init ile inşa edildiği için değiştirilemez
+    //    }
+    //}
+
+    //CLASSLAR SERVİCELER VB . DURUMLARDA TAŞINACAK DATA İMMUTABLE OLMALI
+
+}
---
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microservices.Catalog.Api.Repostories;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using NewMicroservices.Shared;
+using System.Net;
+
+namespace Microservices.Catalog.Api.Features.Categories.Create
+{
+    public class CreateCategoryCommandHandler(AppDbContext context)//mediatR test surecını kolaylaştırır . test için appdbcontext e ihtiyacımız var
+        : IRequestHandler<CreateCategoryCommand,ServiceResult<CreateCategoryResponse>>
+    {
+        public async Task<ServiceResult<CreateCategoryResponse>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            //category adı daha önce kullanılmış mı kontrol et :  mantıken etme yolu karşılaştıma : VAR OLAN LİSTE İLE İSTEĞİ KARŞILAŞTIMA
+            //BİZ DE İSTEK İŞLEMİNİ İŞELYEN CLASSINDAYIZ :  İSTEK CLASSI : CreateCategoryCommand İLE CATEGORY KARŞILAŞTITILACAK
+            var existCategory = context.Categories.AnyAsync(x => x.Name == request.Name, cancellationToken: cancellationToken);
+            //1.Name =Category(kategory listesi classı ) clasından gelir 2.Name : CreateCategoryCommand(ategorı oluşturma ) clsından gelr// any varmı 
+
+
+            if (existCategory)//check etme 
+            {//title detail                                //ERROR MESAJLARINDAN STRING STRING BADREQUEST OLAN ERROR MESAJINI DOLDURDUK /Shared CLASS LIB  ServiceResult CLACC İÇİ
+                ServiceResult<CreateCategoryResponse>.Error("category name already exist",$"the category name '{request.Name}' already exist",
+                    HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
