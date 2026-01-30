f871b6d1aa9c1147dc87c891a910f6f485628173
omeraydin41 <omerenver1317@gmail.com>
2026-01-30 20:27:37 +0300

    Create Generic Endpointi için Resultextension methodu yazıldı

--- Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NewMicroservices.Shared.Extansions;
 
 namespace Microservices.Catalog.Api.Features.Categories.Create
 {
@@ -9,18 +10,9 @@ namespace Microservices.Catalog.Api.Features.Categories.Create
         public static RouteGroupBuilder CreateCategoryGroupItemEndpoint(this RouteGroupBuilder group)
         {
             //http:localhost:5000/api/categories den sonra / alanı gelır ve yonlendirir
-            group.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) =>
-            {// "/" anlamı gurupları kontrol eden CategoryEndpointExt classındaki "api/categories" alanına denk 
+            group.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) => ( await mediator.Send(command)).ToGenericResult());//ToResult methodumuzn adı donuş olarak belirlemdi
+            // "/" anlamı gurupları kontrol eden CategoryEndpointExt classındaki "api/categories" alanına denk 
 
-                var result = await mediator.Send(command);
-
-                return new ObjectResult(result)
-                {
-                    StatusCode = result.Status.GetHashCode(),
-
-                };
-
-            });
             return group;
         }
 
