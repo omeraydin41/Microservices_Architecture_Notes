b04d49dc461428d219c6c808886e935deb9aa267
omeraydin41 <omerenver1317@gmail.com>
2026-02-03 15:15:41 +0300

    Shared library projesi ana dizine taşındı ve eklendi

---
+++ shared/NewMicroservices.Shared/Extansions/CommonServiceExt.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMicroservices.Shared.Extansions
+{
+    public static class CommonServiceExt
+    {
+        //IServiceCollection geriyedönüş tipimiz
+        public static IServiceCollection AddCommonServiceExt(this IServiceCollection services,Type assembly)
+        {
+            services.AddHttpContextAccessor();//yüm servislere erişmek için 
+
+            services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining(assembly));//verilen classı tarar ve bulur 
+
+            return services;//amacımız her mıcreoservicede assembly dışardan vermek 
+        }
+    }
+}
---
+++ shared/NewMicroservices.Shared/Extansions/EndpointResultExt.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMicroservices.Shared.Extansions
+{
+    public static class ResultExt
+    {
+        //controller endpoint Iaction result mınımal api için TResult
+        public static IResult ToGenericResult<T>(this ServiceResult<T> result)
+        {
+            return result.Status switch
+            {
+                HttpStatusCode.OK => Results.Ok(result),
+
+                HttpStatusCode.Created=> Results.Created(result.UrlAsCreated,result),
+
+                HttpStatusCode.NotFound => Results.NotFound(),
+
+                HttpStatusCode.BadRequest => Results.Problem(result.Fail!),
+                //HttpStatusCode.NoContent => Results.NoContent(),
+                _ => Results.Problem(result.Fail!)
+                //Problem hatanın detayını veririz ServiceResult içindeki Fail propertysinden //null olamaz 
+
+
+
+            };
+        }
+
+        public static IResult ToGenericResult(this ServiceResult result)
+        {
+            return result.Status switch
+            {
+                
+
+                HttpStatusCode.NoContent => Results.NoContent(),
+                HttpStatusCode.NotFound => Results.NotFound(),
+
+                HttpStatusCode.BadRequest => Results.Problem(result.Fail!),
+                _ => Results.Problem(result.Fail!)
+                //Problem hatanın detayını veririz ServiceResult içindeki Fail propertysinden //null olamaz 
+
+
+
+            };
+        }
+
+
+    }
+}
