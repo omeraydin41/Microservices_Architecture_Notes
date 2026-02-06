c3569874ff13933fe7764fc94b4a03ef902bd6bf
omeraydin41 <omerenver1317@gmail.com>
2026-02-06 22:00:00 +0300

    Veri tabanına kategori adı girerken boşş isim girilmesine ve yetersiz karakter girilmesini engellemek için fluent validation mantığını uyguladım.bu mantığı categori adı üreten class olan CreateCategoryCommand için CreateCategoryCommandValidator classına fitrasyon ayarı yaptim ve Shared library içinde bulunan ValidationFilter classı içinde abstarct kullanan classları yakalayarak Clean code uygun olarak hatalı categori adı girişlerine karşı bir kod bloğu tanımladım . ve projemde kullanılan FluentValidation paketini program.cs ye direkt yazmak yerine CommonServiceExt üzerinden implemente ettim

---
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Microservices.Catalog.Api.Features.Categories.Create
+{
+
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>//kinin için validasyon yapacağız= CreateCategoryCommand
+    {
+
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("peroperty name it cant empty").
+                Length(4, 25).WithMessage("must be between 4 and 25");
+        }
+    }
+}
--- Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
+++ Microservices.Catalog.Api/Features/Categories/Create/CreateCategoryEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using NewMicroservices.Shared.Extansions;
+using NewMicroservices.Shared.Filters;
 
 namespace Microservices.Catalog.Api.Features.Categories.Create
 {
@@ -10,8 +11,15 @@ namespace Microservices.Catalog.Api.Features.Categories.Create
         public static RouteGroupBuilder CreateCategoryGroupItemEndpoint(this RouteGroupBuilder group)
         {
             //http:localhost:5000/api/categories den sonra / alanı gelır ve yonlendirir
-            group.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) => ( await mediator.Send(command)).ToGenericResult());//ToResult methodumuzn adı donuş olarak belirlemdi
+            group.MapPost("/", async (CreateCategoryCommand command, IMediator mediator) =>
+            (await mediator.Send(command)).ToGenericResult())//ToResult methodumuzn adı donuş olarak belirlemdi
             // "/" anlamı gurupları kontrol eden CategoryEndpointExt classındaki "api/categories" alanına denk 
+
+                .AddEndpointFilter<ValidationFilter<CreateCategoryCommand>>();
+            //end pointe fiter ekleyerek  hangi classın valıdasyona uğryacağı ve validasyon yapan classıda veriyoruz
+            //validasyon yapan class generic olduğundan(ValidationFilter) her endpoinet teker teker yzılmalı ama dinamik olmasaydı direkt 
+            //CategoryEndpointExt classından guruplanan endpoite verilebilirdi 
+
 
             return group;
         }
