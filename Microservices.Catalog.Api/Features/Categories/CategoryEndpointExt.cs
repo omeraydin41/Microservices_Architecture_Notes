80df5c1b90720e5706daa743ce46cc436fd64d76
omeraydin41 <omerenver1317@gmail.com>
2026-02-07 16:16:58 +0300

    -CategoryDto: Verilerin taşınırken değiştirilemez (immutable) olması ve değer temelli karşılaştırma yapılabilmesi için 'record' yapısıyla oluşturdum. -GetAllCategoryQuery: MediatR üzerinden dönecek sonuç tipini (ServiceResult<List<CategoryDto>>) belirleyen sorgu nesnesi olarak tanımladım -GetAllCategoryQueryHandler: Veritabanına (AppDbContext) erişip ham veriyi alan ve bu veriyi DTO'ya dönüştürerek iş mantığını yürüten handler sınıfı olarak ekledim. -GetAllCategoryEndpoint: Minimal API kullanarak api/categories yoluna bir GET isteği bağlandı ve isteği MediatR üzerinden Handler'a yönlendirecek şekilde kurguladım. -CategoryEndpointExt: Tüm kategori endpoint'lerini tek bir merkezden (api/categories) yönetmek ve gruplandırmak için genişletme (extension) metodu olarak yazdım.

--- Microservices.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
+++ Microservices.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
@@ -1,14 +1,18 @@
 ﻿using Microservices.Catalog.Api.Features.Categories.Create;
+using Microservices.Catalog.Api.Features.Categories.GetAll;
 
 namespace Microservices.Catalog.Api.Features.Categories
 {
     public static class CategoryEndpointExt
     {
 
-        public static void AddCategoryGroupEndpointExt(this WebApplication app)
+        public static void AddCategoryGroupEndpointExt(this WebApplication app)//her endpoint buraya eklenmeli 
         {
-            app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint();
+            app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint().
             // grup adı : "api/categories"  CreateCategoryGroupItemEndpoint methodu  CreateCategoryEndpoint clasından gelir
+
+                GetAllCategoryGroupItemEndpoint();
+            // grup adı : "api/categories"  GetAllCategoryGroupItemEndpoint methodu  GetAllCategoryEndpoint clasından gelir
         }
     }
 }
---
+++ Microservices.Catalog.Api/Features/Categories/GetAll/GetAllCategoryEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microservices.Catalog.Api.Features.Categories.Create;
+using Microservices.Catalog.Api.Features.Categories.Dtos;
+using Microservices.Catalog.Api.Repostories;
+using Microsoft.EntityFrameworkCore;
+using NewMicroservices.Shared;
+using NewMicroservices.Shared.Extansions;
+using NewMicroservices.Shared.Filters;
+
+namespace Microservices.Catalog.Api.Features.Categories.GetAll
+{
+    public class GetAllCategoryQuery:IRequest<ServiceResult<List<CategoryDto>>>;
+    //IRequest: MediatR kütüphanesinin bir parçasıdır ve bir sorgu (query) veya komut (command) nesnesi oluşturmak için kullanılır.
+    //ServiceResult üzerinden list şeklinde CategoryDto döneceğiz
+
+
+    //primary constuctounda AppDbContext alırız çünkü veritabanına erişmemiz gerekecek ve bu işlemi yaparken
+    //MediatR'ın sağladığı IRequestHandler arayüzünü kullanarak sorguyu işlemek için bir sınıf oluştururuz.
+    public class GetAllCategoryQueryHandler(AppDbContext context) : IRequestHandler<GetAllCategoryQuery, ServiceResult<List<CategoryDto>>>
+    {//IRequestHandler: MediatR kütüphanesinin bir parçasıdır ve belirli bir sorgu (query) veya komut (command) türünü işlemek için kullanılır.
+        public async Task<ServiceResult<List<CategoryDto>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            
+            var categories= await context.Categories.ToListAsync();
+
+            var categoriesAsDto=categories.Select(c=> new CategoryDto(c.Id,c.Name)).ToList();
+
+            return ServiceResult<List<CategoryDto>>.SuccessAsOk(categoriesAsDto);
+
+        }
+    }
+
+
+
+
+
+    //tüm kategorileri listelem işlemi yapar 
+    public static class GetAllCategoryEndpoint
+    {
+
+        public static RouteGroupBuilder GetAllCategoryGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+
+            group.MapGet("/", async (IMediator mediator) =>
+            (await mediator.Send(new GetAllCategoryQuery())).ToGenericResult());//ToResult methodumuzn adı donuş olarak belirlemdi
+            // "/" anlamı gurupları kontrol eden CategoryEndpointExt classındaki "api/categories" alanına denk 
+
+            //end pointe fiter ekleyerek  hangi classın valıdasyona uğryacağı ve validasyon yapan classıda veriyoruz
+            //validasyon yapan class generic olduğundan(ValidationFilter) her endpoinet teker teker yzılmalı ama dinamik olmasaydı direkt 
+            //CategoryEndpointExt classından guruplanan endpoite verilebilirdi 
+
+
+            return group;
+        }
+
+    }
+}
