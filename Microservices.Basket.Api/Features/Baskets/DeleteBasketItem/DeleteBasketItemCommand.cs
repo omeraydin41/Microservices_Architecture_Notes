c75cc8bbca616c1e0f88ed3aa75142335f64f6e4
omeraydin41 <omerenver1317@gmail.com>
2026-02-28 14:43:40 +0300

    ENG : To delete data from the basket service, DeleteBasketItemCommand and DeleteBasketItemCommandHandler were created. During the process, the user ID is retrieved via IIdentityServices, and the basket data in Redis is queried using IDistributedCache. If the basket is not found, an error is returned; if found, the data is deserialized and converted to BasketDto. The product to be deleted is searched for in the basket using CourseId. When found, it is removed from the list, the updated basket is saved back to Redis, and the process is completed with the NoContent result.
    TUR : Basket servisindeki verileri silmek için DeleteBasketItemCommand ve DeleteBasketItemCommandHandler oluşturuldu. İşlem sırasında IIdentityServices üzerinden kullanıcı ID’si alınarak Redis’teki sepet verisi IDistributedCache ile sorgulanır. Sepet bulunamazsa hata döndürülür; bulunursa veri deserialize edilerek BasketDto’ya çevrilir. Silinmek istenen ürün CourseId ile sepette aranır, bulunduğunda listeden kaldırılır, güncel sepet tekrar Redis’e kaydedilir ve işlem NoContent sonucu ile tamamlanır.

---
+++ Microservices.Basket.Api/Features/Baskets/DeleteBasketItem/DeleteBasketItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using NewMicroservices.Shared;
+
+namespace Microservices.Basket.Api.Features.Baskets.DeleteBasketItem
+{
+    public record DeleteBasketItemCommand(Guid CourseId):IRequestByServiceResult;//IRequestByServiceResult  sahredden gelır 
+                                                                                 //silme işlemi ID y egore yapıldığından CourseId adı diğer alanlarla aynı ada sahıp olmalı 
+
+}
---
+++ Microservices.Basket.Api/Features/Baskets/DeleteBasketItem/DeleteBasketItemCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microservices.Basket.Api.Const;
+using Microservices.Basket.Api.Dto;
+using Microsoft.Extensions.Caching.Distributed;
+using NewMicroservices.Shared;
+using NewMicroservices.Shared.Services;
+using System.Net;
+using System.Text.Json;
+
+namespace Microservices.Basket.Api.Features.Baskets.DeleteBasketItem
+{
+    //db ye işlem için IDistributedCache kulllanılmalı
+    public class DeleteBasketItemCommandHandler(IDistributedCache distributedCache,IIdentityServices identityServices) : IRequestHandler<DeleteBasketItemCommand, ServiceResult>
+    {
+        public async Task<ServiceResult> Handle(DeleteBasketItemCommand request, CancellationToken cancellationToken)
+        {
+            //silme işlemi için önde basket bulunmalı 
+
+            Guid userId = identityServices.GetUserId;//user ıd alındı 
+
+            //basket varmı kontrol edildi 
+            var cacheKey = string.Format(BasketConst.BasketCacheKey, userId);
+            var basketAsString = await distributedCache.GetStringAsync(cacheKey, cancellationToken);
+
+            //basket yoksa geriye hata donulmesi lazım 
+
+            if (string.IsNullOrEmpty(basketAsString))
+            {
+                return ServiceResult.Error("basket not found",HttpStatusCode.NotFound);
+            }
+            //eğer basket varsa deserialize edilmesi lazım : 
+            var currentBasket = JsonSerializer.Deserialize<BasketDto>(basketAsString);
+
+            //silinecek olan basketı bulma işlemi 
+
+            var basketItemToDelete=currentBasket!.BasketItems.FirstOrDefault(x=>x.Id==request.CourseId);
+            if (basketItemToDelete is null)
+            {
+                return ServiceResult.Error("basket ıtem not found",HttpStatusCode.NotFound);
+            }
+            
+            currentBasket.BasketItems.Remove(basketItemToDelete);
+
+            basketAsString=JsonSerializer.Serialize(currentBasket);
+
+            await distributedCache.SetStringAsync(cacheKey,basketAsString,token:cancellationToken);
+            
+            return ServiceResult.SuccessAsNoContent();
+        }
+    }
+}
