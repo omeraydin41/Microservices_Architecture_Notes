01a87c5ceecbd6ba65e4c664de3b11f209cf7281
omeraydin41 <omerenver1317@gmail.com>
2026-03-04 17:40:49 +0300

    ENG : The ApplyDiscountCouponCommand and ApplyDiscountCouponCommandHandler classes have been added to apply a discount to the user's cart.
    TUR : Kullanıcının basketine indirim uygulamak içinApplyDiscountCouponCommand ve  ApplyDiscountCouponCommandHandler classları eklendi.

---
+++ Microservices.Basket.Api/Features/Baskets/ApplyDiscountCoupon/ApplyDiscountCouponCommand.cs
@@ -0,0 +1,7 @@
+﻿using NewMicroservices.Shared;
+
+namespace Microservices.Basket.Api.Features.Baskets.ApplyDiscountCoupon
+{
+    public record ApplyDiscountCouponCommand(string Coupon,float DiscountRate):IRequestByServiceResult;
+   
+}
---
+++ Microservices.Basket.Api/Features/Baskets/ApplyDiscountCoupon/ApplyDiscountCouponCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microservices.Basket.Api.Const;
+using Microservices.Basket.Api.Dto;
+using Microsoft.Extensions.Caching.Distributed;
+using NewMicroservices.Shared;
+using NewMicroservices.Shared.Services;
+using System.Net;
+using System.Text.Json;
+
+namespace Microservices.Basket.Api.Features.Baskets.ApplyDiscountCoupon
+{
+    public class ApplyDiscountCouponCommandHandler(IIdentityServices identityServices,IDistributedCache distributedCache) 
+        : IRequestHandler<ApplyDiscountCouponCommand, ServiceResult>//handler edileceek class ve gerşye donş turü
+    {
+        public async Task<ServiceResult> Handle(ApplyDiscountCouponCommand request, CancellationToken cancellationToken) 
+        {
+            //indirim için öncesepettekı datayı çektik
+            var cacheKey = string.Format(BasketConst.BasketCacheKey, identityServices.GetUserId);
+            var basketAsJson=await distributedCache.GetStringAsync(cacheKey,token:cancellationToken);
+
+            //data yoksa :
+            if (string.IsNullOrEmpty(basketAsJson))
+            {
+                return ServiceResult<BasketDto>.Error("basket not found", HttpStatusCode.NotFound);
+            }
+            //bunu çevirme varkti:
+            var basket = JsonSerializer.Deserialize<Data.Basket>(basketAsJson);
+            basket.ApplyNewDiscount(request.Coupon, request.DiscountRate);
+
+            basketAsJson = JsonSerializer.Serialize(basket);
+            await distributedCache.SetStringAsync(cacheKey, basketAsJson, token:cancellationToken);
+
+            return ServiceResult.SuccessAsNoContent();
+        }
+    }
+}
