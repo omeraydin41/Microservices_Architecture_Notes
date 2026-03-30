c49872217474da9c75889d24066a88640ad6dad7
omeraydin41 <omerenver1317@gmail.com>
2026-03-30 21:22:37 +0300

    ENG : For the design of the Domain layer, which serves as the core layer, the BaseEntity class was added to the MicroserviceOrder.Domain Class Library as the primary entity, and the OrderItem class was included to illustrate the concepts of anemic models and rich domain models.
    TUR : Cekirdek katman olan Domain katmani tasarimi icin MicroserviceOrder.Domain Class Library icine ana varlık BaseEntitiy ve anemic model ve Rich Domain Model mantığını anlamak için OrderItem classi eklendi.

---
+++ MicroserviceOrder.Domain/Entities/OrderItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceOrder.Domain.Entities
+{ //anemic model => rich domain modele çevrilme amaçlanır(yardımcı method eklenmesi)
+    public class OrderItem : BaseEntity<int>
+    {//OrderItem keni çapında olduğunden int olabilir ama ORDERİN KENİSİ GUİD
+
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = default!;//null olamaz dolmak zorunda 
+        public decimal UnitPrice { get; set; }
+
+
+
+        #region
+        //rich domain : bunlar behavior methotdur
+
+        public void SetItem(Guid ProductId, string ProductName, decimal UnitPrice)
+        {
+            if (string.IsNullOrEmpty(ProductName))
+            {
+                throw new ArgumentNullException("ProductName can not is empty");
+            }
+            if (UnitPrice<=0)
+            {
+                throw new ArgumentNullException("UnitPrice can not be less than equal the zero.");
+            }
+
+            this.ProductId=ProductId;
+            this.ProductName=ProductName;
+            this.UnitPrice=UnitPrice;
+        }
+
+        //fiyatı güncelleme
+        public void UpdataPrice(decimal newPrice)
+        {
+            if (newPrice <= 0) { throw new ArgumentNullException("UnitPrice cannot be less than or equal zero");}
+            this.UnitPrice = newPrice;
+        }
+
+        //indirim uygulama 
+        public void ApplyDiscount(double discountPercentage)
+        {
+            if (discountPercentage<0||discountPercentage>100)
+            {
+                throw new ArgumentNullException("Discount Percentage must be between 0 and 100.");
+            }
+            this.UnitPrice -= (this.UnitPrice * (decimal)discountPercentage / 100);
+        }
+
+        public bool IsSameItem(OrderItem otherIthem)
+        {
+            return this.ProductId == otherIthem.ProductId;
+        }
+        #endregion
+
+    }
+}
